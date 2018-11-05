7ced4dbf51908140dc338f5125325a7e8f71badb
smadeux <scott.madeux@gmail.com>
2018-11-05 14:51:17 -0700

    First Commit by Scott

--- app/src/main/java/com/example/chinesetutorialapp/MainActivity.java
+++ app/src/main/java/com/example/chinesetutorialapp/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //Hey I'm making a comment
        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
